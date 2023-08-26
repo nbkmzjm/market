27b0a6887618d7141a30e2558b87b1af43a82314
thienngo <ngokhanhthien@yahoo.com>
2023-08-26 00:54:14 -0700

    add to store buttion visibillity

--- frontend/src/components/Modal/EditProductModal.js
+++ frontend/src/components/Modal/EditProductModal.js
@@ -5,161 +5,160 @@ import { db } from '../../config/firebase';
 import { toast } from 'react-toastify';
 
 function EditProductModal({ product, showModal, modalClose }) {
-   const [show, setShow] = useState();
+  const [show, setShow] = useState();
 
-   const [editedProduct, setEditedProduct] = useState(product);
+  const [editedProduct, setEditedProduct] = useState(product);
 
-   const handleInputChange = (event) => {
-      const { name, value } = event.target;
-      setEditedProduct({ ...editedProduct, [name]: value });
-   };
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setEditedProduct({ ...editedProduct, [name]: value });
+  };
 
-   const handleClose = () => {
-      setShow(false);
-      modalClose(false);
-   };
+  const handleClose = () => {
+    setShow(false);
+    modalClose(false);
+  };
 
-   useEffect(() => {
-      setEditedProduct(product);
-      setShow(showModal);
-   }, [showModal, product]);
+  useEffect(() => {
+    setEditedProduct(product);
+    setShow(showModal);
+  }, [showModal, product]);
 
-   // const handleShow = () => setShow(true);
+  // const handleShow = () => setShow(true);
 
-   const handleSave = async () => {
-      console.log(editedProduct);
-      const productRef = doc(db, 'product', editedProduct.id);
+  const handleSave = async () => {
+    console.log(editedProduct);
+    const productRef = doc(db, 'product', editedProduct.id);
 
-      try {
-         await updateDoc(productRef, editedProduct);
-         toast.success('Product updated successfully');
-         handleClose();
-      } catch (error) {
-         console.log(error.message);
-         toast.error('Cannot update product', error.message);
-      }
-   };
+    try {
+      await updateDoc(productRef, editedProduct);
+      toast.success('Product updated successfully');
+      handleClose();
+    } catch (error) {
+      console.log(error.message);
+      toast.error('Cannot update product', error.message);
+    }
+  };
 
-   return (
-      <div>
-         {console.log(show)}
-         {console.log(editedProduct)}
-         <Modal show={show} onHide={handleClose} scrollable>
-            <Modal.Header closeButton>
-               <Modal.Title>Edit Product</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-               {/* <EditProductForm product={product} onSave={handleSave} /> */}
-               <div>
-                  {
-                     <form>
-                        <div className="mb-3">
-                           <label>Name</label>
-                           <input
-                              type="text"
-                              name="name"
-                              value={editedProduct.name}
-                              onChange={handleInputChange}
-                              className="form-control"
-                           />
-                        </div>
-                        <div className="mb-3">
-                           <label>Brand</label>
-                           <input
-                              type="text"
-                              name="brand"
-                              value={editedProduct.brand}
-                              onChange={handleInputChange}
-                              className="form-control"
-                           />
-                        </div>
-                        <div className="mb-3">
-                           <label>Category</label>
-                           <input
-                              type="text"
-                              name="category"
-                              value={editedProduct.category}
-                              onChange={handleInputChange}
-                              className="form-control"
-                           />
-                        </div>
-                        <div className="mb-3">
-                           <label>Description</label>
-                           <input
-                              type="text"
-                              name="description"
-                              value={editedProduct.description}
-                              onChange={handleInputChange}
-                              className="form-control"
-                           />
-                        </div>
-                        <div className="mb-3">
-                           <label>Note</label>
-                           <input
-                              type="text"
-                              name="Note"
-                              value={editedProduct.Note}
-                              onChange={handleInputChange}
-                              className="form-control"
-                           />
-                        </div>
-                        <div className="mb-3">
-                           <label>Rating</label>
-                           <input
-                              type="text"
-                              name="ratings"
-                              value={editedProduct.ratings}
-                              onChange={handleInputChange}
-                              className="form-control"
-                           />
-                        </div>
-                        <div className="mb-3">
-                           <label>Size</label>
-                           <input
-                              type="text"
-                              name="size"
-                              value={editedProduct.size}
-                              onChange={handleInputChange}
-                              className="form-control"
-                           />
-                        </div>
-                        <div className="mb-3">
-                           <label>Unit</label>
-                           <input
-                              type="text"
-                              name="unit"
-                              value={editedProduct.unit}
-                              onChange={handleInputChange}
-                              className="form-control"
-                           />
-                        </div>
-                        <div className="mb-3">
-                           <label>slug</label>
-                           <input
-                              type="text"
-                              name="slug"
-                              value={editedProduct.slug}
-                              onChange={handleInputChange}
-                              className="form-control"
-                           />
-                        </div>
-                        {/* Add other input fields for category, description, note, image, slug, size, unit, and rating */}
-                     </form>
-                  }
-               </div>
-            </Modal.Body>
-            <Modal.Footer>
-               <Button variant="primary" onClick={handleSave}>
-                  {' '}
-                  Save{' '}
-               </Button>
-               <Button variant="secondary" onClick={handleClose}>
-                  Close
-               </Button>
-            </Modal.Footer>
-         </Modal>
-      </div>
-   );
+  return (
+    <div>
+      {console.log(show)}
+      {console.log(editedProduct)}
+      <Modal show={show} onHide={handleClose} scrollable>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Product</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/* <EditProductForm product={product} onSave={handleSave} /> */}
+          <div>
+            {
+              <form>
+                <div className="mb-3">
+                  <label>Name</label>
+                  <input
+                    type="text"
+                    name="name"
+                    value={editedProduct.name}
+                    onChange={handleInputChange}
+                    className="form-control"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label>Brand</label>
+                  <input
+                    type="text"
+                    name="brand"
+                    value={editedProduct.brand}
+                    onChange={handleInputChange}
+                    className="form-control"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label>Category</label>
+                  <input
+                    type="text"
+                    name="category"
+                    value={editedProduct.category}
+                    onChange={handleInputChange}
+                    className="form-control"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label>Description</label>
+                  <input
+                    type="text"
+                    name="description"
+                    value={editedProduct.description}
+                    onChange={handleInputChange}
+                    className="form-control"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label>Note</label>
+                  <input
+                    type="text"
+                    name="Note"
+                    value={editedProduct.Note}
+                    onChange={handleInputChange}
+                    className="form-control"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label>Rating</label>
+                  <input
+                    type="text"
+                    name="ratings"
+                    value={editedProduct.ratings}
+                    onChange={handleInputChange}
+                    className="form-control"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label>Size</label>
+                  <input
+                    type="text"
+                    name="size"
+                    value={editedProduct.size}
+                    onChange={handleInputChange}
+                    className="form-control"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label>Unit</label>
+                  <input
+                    type="text"
+                    name="unit"
+                    value={editedProduct.unit}
+                    onChange={handleInputChange}
+                    className="form-control"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label>slug</label>
+                  <input
+                    type="text"
+                    name="slug"
+                    value={editedProduct.slug}
+                    onChange={handleInputChange}
+                    className="form-control"
+                  />
+                </div>
+                {/* Add other input fields for category, description, note, image, slug, size, unit, and rating */}
+              </form>
+            }
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleSave}>
+            Save
+          </Button>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
 
 export default EditProductModal;
--- frontend/src/screens/ProductTemplateScreen.js
+++ frontend/src/screens/ProductTemplateScreen.js
@@ -1,80 +1,106 @@
-import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react';
+import { addDoc, collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import React, { useContext, useEffect, useState } from 'react';
 import { db } from '../config/firebase';
 import Button from 'react-bootstrap/esm/Button';
 import EditProductModal from '../components/Modal/EditProductModal';
+import { Store } from '../Store';
+import { toast } from 'react-toastify';
 
 export default function ProductTemplateScreens() {
-   const [products, setProducts] = useState([]);
-   const [product, setProduct] = useState({});
-   const [showModal, setShowModal] = useState(false);
+  const { state, dispatch } = useContext(Store);
 
-   //  useEffect(() => {
-   //     const fetchProducts = async () => {
-   //        const productArray = [];
-   //        const snapshot = await getDocs().collection('products').get();
-   //        snapshot.forEach((doc) => {
-   //           const product = { ...doc.data(), id: doc.id };
-   //           productArray.push(product);
-   //        });
+  const [products, setProducts] = useState([]);
+  const [product, setProduct] = useState({});
+  const [showModal, setShowModal] = useState(false);
 
-   //        setProducts(productArray);
-   //     };
+  //  useEffect(() => {
+  //     const fetchProducts = async () => {
+  //        const productArray = [];
+  //        const snapshot = await getDocs().collection('products').get();
+  //        snapshot.forEach((doc) => {
+  //           const product = { ...doc.data(), id: doc.id };
+  //           productArray.push(product);
+  //        });
 
-   //     fetchProducts();
-   //  }, []);
+  //        setProducts(productArray);
+  //     };
 
-   useEffect(() => {
-      const fetchProducts = async () => {
-         const productArray = [];
-         const querySnapshot = await getDocs(collection(db, 'product'));
-         if (!querySnapshot.empty) {
-            querySnapshot.forEach((doc) => {
-               const product = { ...doc.data(), id: doc.id };
-               productArray.push(product);
-            });
-         }
+  //     fetchProducts();
+  //  }, []);
 
-         setProducts(productArray);
-      };
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const productArray = [];
+      const querySnapshot = await getDocs(collection(db, 'product'));
+      if (!querySnapshot.empty) {
+        querySnapshot.forEach((doc) => {
+          const product = { ...doc.data(), id: doc.id };
+          productArray.push(product);
+        });
+      }
 
-      fetchProducts();
-   }, []);
-   const setShowModalHandler = (boolean) => {
-      setShowModal(boolean);
-   };
-   const editProductHandler = (productId) => {
-      console.log(productId);
-      setShowModal(true);
-      const fetchProduct = async () => {
-         const snapshot = await getDoc(doc(db, 'product', productId));
-         if (snapshot.exists()) {
-            console.log('Document data:', snapshot.data());
-            setProduct({ ...snapshot.data(), id: snapshot.id });
-         } else {
-            console.log('No such document!');
-         }
-      };
-      fetchProduct();
-   };
+      setProducts(productArray);
+    };
 
-   const addToStoreHandler = () => {};
+    fetchProducts();
+  }, [showModal]);
+  const setShowModalHandler = (boolean) => {
+    setShowModal(boolean);
+  };
+  const editProductHandler = (productId) => {
+    console.log(productId);
+    setShowModal(true);
+    const fetchProduct = async () => {
+      const snapshot = await getDoc(doc(db, 'product', productId));
+      if (snapshot.exists()) {
+        console.log('Document data:', snapshot.data());
+        setProduct({ ...snapshot.data(), id: snapshot.id });
+      } else {
+        console.log('No such document!');
+      }
+    };
+    fetchProduct();
+  };
 
-   return (
-      <div className="container">
-         <EditProductModal
-            showModal={showModal}
-            product={product}
-            modalClose={setShowModalHandler}
-         />
-         <div className="row">
-            <div className="col-12">
-               <div className="card-header">
-                  <h5 className="">Product</h5>
-               </div>
-               <div className="card-body">
-                  <ul className="list-group list-group-flush">
-                     {/* <li className="list-group-item">
+  const productAdded = async (productId) => {
+    console.log(productId);
+  };
+
+  const addToStoreHandler = async (productId) => {
+    try {
+      const productSnap = await getDoc(doc(db, 'product', productId));
+      if (productSnap.exists()) {
+        const productData = {
+          ...productSnap.data(),
+          supplierId: state.userInfo.uid,
+          templateId: productSnap.id,
+        };
+        await addDoc(collection(db, 'retailProduct'), productData);
+
+        toast.success('Product added to store!');
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Can not add product to store!');
+    }
+  };
+
+  return (
+    <div className="container">
+      {console.log('render template screens')}
+      <EditProductModal
+        showModal={showModal}
+        product={product}
+        modalClose={setShowModalHandler}
+      />
+      <div className="row">
+        <div className="col-12">
+          <div className="card-header">
+            <h5 className="">Product</h5>
+          </div>
+          <div className="card-body">
+            <ul className="list-group list-group-flush">
+              {/* <li className="list-group-item">
                         <div className="row">
                            <div className="col-2"></div>
                            <div className="col-6">Product</div>
@@ -82,45 +108,44 @@ export default function ProductTemplateScreens() {
                            <div className="col-2">Total Price</div>
                         </div>
                      </li> */}
-                     {products.map((product) => (
-                        <li key={product.id} className="list-group-item">
-                           <div className="row">
-                              <div className="col-2">
-                                 <img
-                                    src={product.image}
-                                    alt={product.name}
-                                    height="100"
-                                    width="100"
-                                    className="img-fluid rounded img-thumbnail"
-                                 ></img>{' '}
-                              </div>
-                              <div className="col-6">{product.name}</div>
-                              <div className="col-2">
-                                 <Button
-                                    variant="primary"
-                                    onClick={() =>
-                                       editProductHandler(product.id)
-                                    }
-                                 >
-                                    Edit
-                                 </Button>
-                              </div>
-                              <div className="col-2">
-                                 {' '}
-                                 <Button
-                                    variant="primary"
-                                    onClick={addToStoreHandler}
-                                 >
-                                    Add to Store
-                                 </Button>
-                              </div>
-                           </div>
-                        </li>
-                     ))}
-                  </ul>
-               </div>
-            </div>
-         </div>
+              {products.map((product) => (
+                <li key={product.id} className="list-group-item">
+                  <div className="row">
+                    <div className="col-2">
+                      <img
+                        src={product.image}
+                        alt={product.name}
+                        height="100"
+                        width="100"
+                        className="img-fluid rounded img-thumbnail"
+                      ></img>{' '}
+                    </div>
+                    <div className="col-6">{product.name}</div>
+                    <div className="col-2">
+                      <Button
+                        variant="primary"
+                        onClick={() => editProductHandler(product.id)}
+                      >
+                        Edit
+                      </Button>
+                    </div>
+                    <div className="col-2">
+                      {productAdded(product.id)}
+
+                      <Button
+                        variant="primary"
+                        onClick={() => addToStoreHandler(product.id)}
+                      >
+                        Add to Store
+                      </Button>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
-   );
+    </div>
+  );
 }
