304857433af4d7330f0f1706276b48f99d5163fa
thienngo <ngokhanhthien@yahoo.com>
2023-10-03 15:32:47 -0700

    add to cart from wish lish

--- frontend/src/components/InventoryMonitor.js
+++ frontend/src/components/InventoryMonitor.js
@@ -1,13 +1,13 @@
 import {
-   addDoc,
-   collection,
-   doc,
-   getDocs,
-   onSnapshot,
-   query,
-   snapshotEqual,
-   updateDoc,
-   where,
+  addDoc,
+  collection,
+  doc,
+  getDocs,
+  onSnapshot,
+  query,
+  snapshotEqual,
+  updateDoc,
+  where,
 } from 'firebase/firestore';
 import React, { useContext, useEffect, useState } from 'react';
 import { db } from '../config/firebase';
@@ -15,98 +15,98 @@ import { Store } from '../Store';
 import { roundNumber } from '../functions/calculation';
 
 export default function InventoryMonitor() {
-   const { state } = useContext(Store);
-   console.log('state:', state.userInfo.account.accountId);
-   const accountId = state.userInfo.account.accountId;
-   const [item, setItem] = useState([]);
-   console.log('item:', item);
+  const { state } = useContext(Store);
+  console.log('state:', state.userInfo.account.accountId);
+  const accountId = state.userInfo.account.accountId;
+  const [item, setItem] = useState([]);
+  console.log('item:', item);
 
-   useEffect(() => {
-      const inventoryRef = collection(
-         db,
-         'accounts',
-         accountId,
-         'accountProducts'
-      );
+  useEffect(() => {
+    const inventoryRef = collection(
+      db,
+      'accounts',
+      accountId,
+      'accountProducts'
+    );
 
-      const unsubscribe = onSnapshot(inventoryRef, (querySnapshot) => {
-         console.log('querySnapshot:', querySnapshot.docChanges());
+    const unsubscribe = onSnapshot(inventoryRef, (querySnapshot) => {
+      console.log('querySnapshot:', querySnapshot.docChanges());
 
-         // const changes = [];
-         // querySnapshot.forEach((doc) => {
-         //    changes.push(doc.data());
-         //    console.log('changes:', doc.type);
-         // });
+      // const changes = [];
+      // querySnapshot.forEach((doc) => {
+      //    changes.push(doc.data());
+      //    console.log('changes:', doc.type);
+      // });
 
-         // console.log('changes:', changes);
+      // console.log('changes:', changes);
 
-         querySnapshot.docChanges().forEach((change) => {
-            console.log(change.type);
-            if (change.type === 'modified') {
-               // Handle the case when an item's inventory is modified (decreased)
-               let updatedProduct = change.doc.data();
-               setItem(updatedProduct);
+      querySnapshot.docChanges().forEach((change) => {
+        console.log(change.type);
+        if (change.type === 'modified') {
+          // Handle the case when an item's inventory is modified (decreased)
+          let updatedProduct = change.doc.data();
+          setItem(updatedProduct);
 
-               if (updatedProduct.countInStock < updatedProduct.min) {
-                  const quantity = roundNumber(
-                     (updatedProduct.max - updatedProduct.min) * 0.7,
-                     0
+          if (updatedProduct.countInStock < updatedProduct.min) {
+            const quantity = roundNumber(
+              (updatedProduct.max - updatedProduct.countInStock) * 0.5,
+              0
+            );
+            updatedProduct = { ...updatedProduct, quantity: quantity };
+            console.log('updated product:', updatedProduct);
+            const updateOrderList = async () => {
+              try {
+                const q = query(
+                  collection(db, 'accounts', accountId, 'orderList'),
+                  where('productId', '==', updatedProduct.productId)
+                );
+                const querySnapOderList = await getDocs(q);
+                console.log('snap order list:', querySnapOderList.size);
+                if (querySnapOderList.size > 0) {
+                  const exitedProductId = querySnapOderList.docs[0].id;
+                  console.log('exitedProductId:', exitedProductId);
+                  const updateProductRef = doc(
+                    db,
+                    'accounts',
+                    accountId,
+                    'orderList',
+                    exitedProductId
                   );
-                  updatedProduct = { ...updatedProduct, quantity: quantity };
-                  console.log('updated product:', updatedProduct);
-                  const updateOrderList = async () => {
-                     try {
-                        const q = query(
-                           collection(db, 'accounts', accountId, 'orderList'),
-                           where('productId', '==', updatedProduct.productId)
-                        );
-                        const querySnapOderList = await getDocs(q);
-                        console.log('snap order list:', querySnapOderList.size);
-                        if (querySnapOderList.size > 0) {
-                           const exitedProductId = querySnapOderList.docs[0].id;
-                           console.log('exitedProductId:', exitedProductId);
-                           const updateProductRef = doc(
-                              db,
-                              'accounts',
-                              accountId,
-                              'orderList',
-                              exitedProductId
-                           );
-                           await updateDoc(updateProductRef, updatedProduct);
-                        } else {
-                           console.log('added');
-                           const collectionRef = collection(
-                              db,
-                              'accounts',
-                              accountId,
-                              'orderList'
-                           );
+                  await updateDoc(updateProductRef, updatedProduct);
+                } else {
+                  console.log('added');
+                  const collectionRef = collection(
+                    db,
+                    'accounts',
+                    accountId,
+                    'orderList'
+                  );
 
-                           await addDoc(collectionRef, updatedProduct);
-                        }
-                     } catch (error) {
-                        console.log(error);
-                     }
-                  };
-                  updateOrderList();
-               }
-               // onInventoryUpdate();
+                  await addDoc(collectionRef, updatedProduct);
+                }
+              } catch (error) {
+                console.log(error);
+              }
+            };
+            updateOrderList();
+          }
+          // onInventoryUpdate();
 
-               // Check if you need to reorder items based on some condition
-               //   if (itemData.quantity <= reorderThreshold) {
-               //     // Call a function to place an order for more items
-               //     placeOrder(itemData.itemId);
-               //   }
-            }
-         });
+          // Check if you need to reorder items based on some condition
+          //   if (itemData.quantity <= reorderThreshold) {
+          //     // Call a function to place an order for more items
+          //     placeOrder(itemData.itemId);
+          //   }
+        }
       });
-      return () => unsubscribe();
-   }, []);
-   return (
-      <div>
-         InventoryMonitor:x
-         {console.log('itemx:', item.countInStock)}
-         {item && <p> {item.countInStock}</p>}
-      </div>
-   );
+    });
+    return () => unsubscribe();
+  }, []);
+  return (
+    <div>
+      InventoryMonitor:x
+      {console.log('itemx:', item.countInStock)}
+      {item && <p> {item.countInStock}</p>}
+    </div>
+  );
 }
--- frontend/src/screens/WishListScreen.js
+++ frontend/src/screens/WishListScreen.js
@@ -3,160 +3,157 @@ import React, { useContext, useEffect, useReducer, useState } from 'react';
 import { useAccordionButton } from 'react-bootstrap';
 import { db } from '../config/firebase';
 import { Store } from '../Store';
-import { getError } from '../utils';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { toast } from 'react-toastify';
+import { getError } from '../functions/utils';
 
 export default function WishListScreen() {
-   const { state, ctxDispatch } = useContext(Store);
-   const { cart } = state;
-   console.log('state', state);
-   const accountId = state.userInfo.account.accountId;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart } = state;
+  console.log('state', state);
+  const accountId = state.userInfo.account.accountId;
 
-   const reducer = (state, action) => {
-      switch (action.type) {
-         case 'FETCH_REQUEST':
-            return { ...state, loading: true, products: [] };
-         case 'FETCH_SUCCESS':
-            return { ...state, loading: false, products: action.payload };
-         case 'FETCH_FAIL':
-            return { ...state, loading: false, error: action.payload };
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'FETCH_REQUEST':
+        return { ...state, loading: true, products: [] };
+      case 'FETCH_SUCCESS':
+        return { ...state, loading: false, products: action.payload };
+      case 'FETCH_FAIL':
+        return { ...state, loading: false, error: action.payload };
+    }
+  };
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    products: [],
+    loading: true,
+    error: '',
+  });
+
+  const addToCardHandler = async (product) => {
+    console.log('adding to cart');
+    console.log(product);
+
+    const existItem = cart.cartItems.find(
+      (x) => x.templateId === product.productId
+    );
+
+    console.log('existItem', existItem);
+
+    //  const quantity = existItem ? existItem.quantity + 1 : 1;
+
+    const q = query(
+      collection(
+        db,
+        'accounts',
+        state.userInfo.account.defaultSupplier.id,
+        'accountProducts'
+      ),
+      where('productId', '==', product.productId)
+    );
+
+    const docSnap = await getDocs(q);
+    console.log(state.userInfo.account.accountId);
+    console.log(docSnap);
+
+    if (docSnap.size > 0) {
+      if (docSnap.docs[0].data().countInStock < product.quantity) {
+        toast('Sorry. Product is out of stock');
+        return;
       }
-   };
-   const [{ loading, error, products }, dispatch] = useReducer(reducer, {
-      products: [],
-      loading: true,
-      error: '',
-   });
+    } else {
+      toast.error('Supplier do not carry this product');
+    }
 
-   const addToCardHandler = async (product) => {
-      console.log('adding to cart');
-      console.log(product);
+    ctxDispatch({
+      type: 'CARD_ADD_ITEM',
+      payload: { ...product, quantity: product.quantity },
+    });
+  };
 
-      const existItem = cart.cartItems.find(
-         (x) => x.templateId === product.templateId
-      );
+  const removeItemHandler = (item) => {};
 
-      const quantity = existItem ? existItem.quantity + 1 : 1;
+  console.log('products', products);
+  useEffect(() => {
+    const fetchData = async () => {
+      const collectionRef = collection(db, 'accounts', accountId, 'orderList');
+      dispatch({ type: 'FETCH_REQUEST' });
 
-      const q = query(
-         collection(
-            db,
-            'accounts',
-            state.userInfo.account.defaultSupplier.id,
-            'accountProducts'
-         ),
-         where('productId', '==', product.productId)
-      );
-
-      const docSnap = await getDocs(q);
-      console.log(state.userInfo.account.accountId);
-      console.log(docSnap);
-
-      if (docSnap.size === 1) {
-         if (docSnap.docs[0].data().countInStock < quantity) {
-            toast('Sorry. Product is out of stock');
-            return;
-         }
-      } else {
-         toast.error('Number of document retuned is incorrect');
+      try {
+        const snapShot = await getDocs(collectionRef);
+        if (snapShot.size > 0) {
+          const productData = snapShot.docs.map((product) => ({
+            ...product.data(),
+            id: product.id,
+          }));
+          console.log('productData', productData);
+          dispatch({ type: 'FETCH_SUCCESS', payload: productData });
+        } else {
+          throw new Error('Cannot find product in wish list.');
+        }
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(error) });
+        console.log(error);
       }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className="container">
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox></MessageBox>
+      ) : (
+        <div>
+          {' '}
+          <div className="card-body">
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <div className="row">
+                  <div className="col-4">Description</div>
+                  <div className="col-2">Quantity</div>
+                  <div className="col-2">Price</div>
+                  <div className="col-2">Remove</div>
+                  <div className="col-2">Cart</div>
+                </div>
+              </li>
+              {products.map((product) => (
+                <li className="list-group-item">
+                  {' '}
+                  <div className="row">
+                    <div className="col-4">{product.name}</div>
+                    <div className="col-2">{product.quantity}</div>
+                    <div className="col-2">{product.price}</div>
 
-      ctxDispatch({
-         type: 'CARD_ADD_ITEM',
-         payload: { ...product, quantity: quantity },
-      });
-   };
-
-   const removeItemHandler = (item) => {};
-
-   console.log('products', products);
-   useEffect(() => {
-      const fetchData = async () => {
-         const collectionRef = collection(
-            db,
-            'accounts',
-            accountId,
-            'orderList'
-         );
-         dispatch({ type: 'FETCH_REQUEST' });
-
-         try {
-            const snapShot = await getDocs(collectionRef);
-            if (snapShot.size > 0) {
-               const productData = snapShot.docs.map((product) => ({
-                  ...product.data(),
-                  id: product.id,
-               }));
-               console.log('productData', productData);
-               dispatch({ type: 'FETCH_SUCCESS', payload: productData });
-            } else {
-               throw new Error('Cannot find product in wish list.');
-            }
-         } catch (error) {
-            dispatch({ type: 'FETCH_FAIL', payload: getError(error) });
-            console.log(error);
-         }
-      };
-      fetchData();
-   }, []);
-   return (
-      <div className="container">
-         {loading ? (
-            <LoadingBox></LoadingBox>
-         ) : error ? (
-            <MessageBox></MessageBox>
-         ) : (
-            <div>
-               {' '}
-               <div className="card-body">
-                  <ul className="list-group list-group-flush">
-                     <li className="list-group-item">
-                        <div className="row">
-                           <div className="col-4">Description</div>
-                           <div className="col-2">Quantity</div>
-                           <div className="col-2">Price</div>
-                           <div className="col-2">Remove</div>
-                           <div className="col-2">Cart</div>
-                        </div>
-                     </li>
-                     {products.map((product) => (
-                        <li className="list-group-item">
-                           {' '}
-                           <div className="row">
-                              <div className="col-4">{product.name}</div>
-                              <div className="col-2">{product.quantity}</div>
-                              <div className="col-2">{product.price}</div>
-
-                              <div className="col-2">
-                                 {' '}
-                                 <button
-                                    type="button"
-                                    className="btn btn-primary"
-                                    variant="light"
-                                    onClick={() => removeItemHandler(product)}
-                                 >
-                                    <i className="fas fa-trash"></i>
-                                 </button>
-                              </div>
-                              <div className="col-2">
-                                 <button
-                                    type="button"
-                                    className="btn btn-primary"
-                                    variant="light"
-                                    onClick={() => addToCardHandler(product)}
-                                 >
-                                    <i class="fas fa-plus"></i>
-                                 </button>
-                              </div>
-                           </div>{' '}
-                        </li>
-                     ))}
-                  </ul>
-               </div>
-            </div>
-         )}
-      </div>
-   );
+                    <div className="col-2">
+                      {' '}
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        variant="light"
+                        onClick={() => removeItemHandler(product)}
+                      >
+                        <i className="fas fa-trash"></i>
+                      </button>
+                    </div>
+                    <div className="col-2">
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        variant="light"
+                        onClick={() => addToCardHandler(product)}
+                      >
+                        <i class="fas fa-plus"></i>
+                      </button>
+                    </div>
+                  </div>{' '}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
